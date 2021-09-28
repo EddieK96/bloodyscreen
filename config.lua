32b00e9df55ab4898e36effa9d4dbec2908bb997
EddieK96 <69137506+EddieK96@users.noreply.github.com>
2021-09-28 12:47:17 +0200

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,84 @@
+-------------------------------------------------------------------------------------------------------------
+	--CONFIG--
+-------------------------------------------------------------------------------------------------------------
+config = {}
+
+	--Pulsating screeneffect as a permanent health indicator:
+	config.activatepulsatingindicator = true
+	
+	--Accute health indicator:
+	config.activateaccutehealthindicator = true
+	
+	--Static screeneffect as a permanent health indicator:
+	config.activatestatichealthindicator = true
+
+	--Maximum effect strength:
+	config.maxeffectstrength = 0.9
+	
+
+-------------------------------------------------------------------------------------------------------------
+	--STATIC HEALTH INDICATOR
+-------------------------------------------------------------------------------------------------------------
+	
+	--At what percantage of health (minus deadhealth*) should the static effect start? (0.0 - 1.0):
+	config.percentagestaticeffecthealthfraction = 1.0
+	
+	--Effect strength multiplier for static health indicator:
+	config.staticindicatorstrengthmultiplier = 0.3
+	
+	
+-------------------------------------------------------------------------------------------------------------
+	--ACCUTE / IMPACT DAMAGE INDICATOR
+-------------------------------------------------------------------------------------------------------------
+	
+	--At what percantage of health (minus deadhealth*) should impact effects start? (0.0 - 1.0):
+	config.percentageimpacteffecthealthfraction = 1.0
+	
+	--Adds a screeneffect ontop of the bloody edges.
+	config.impacteffectboosteffect = false
+	
+	--Effect strength multiplier for damage impacts:
+	config.impacteffectstrengthmultiplier = 14.0
+
+	--Value between 0.0 and 1.0 for how fast the screen effect fades away after a damage impact:
+	config.accutedamagerecoveryrate = 0.02
+
+
+-------------------------------------------------------------------------------------------------------------
+	--PULSATING SCREENEFFECT
+-------------------------------------------------------------------------------------------------------------
+
+	--At what percantage of health (minus deadhealth*) should the pulsating effect start? (0.0 - 1.0):
+	config.percentagepulsatingeffecthealthfraction = 0.49
+	
+	--Adds a screeneffect ontop of the bloody edges:
+	config.pulsatingboosteffect = true
+	
+	-- At what percentage the screeneffect starts playing:
+	config.effectthreshold = 0.3
+	
+	--Variable interval for pulse indicator depending on health. (lower value -> faster):
+	config.minpulseinterval = 30
+	config.maxpulseinterval = 133
+
+	--Effect strength multiplier for pulsating effect:
+	config.pulsatingmultiplier = 0.3
+	
+	--Heartbeat audio when screen is pulsating:
+	config.pulsatingheartbeataudio = true
+	
+	--Heartbeat volume:
+	config.pulsatingheartbeatvolume = 0.8
+	
+	--Pulse recovery rate multiplier:
+	config.pulserecoveryratemultiplier = 1.5
+	
+	
+-- *: Players usally die at a health below 100, not 0. That value is subtracted in the code to get a better relative 
+--    value towards the maximum health.
+
+
+-------------------------------------------------------------------------------------------------------------
+	--END CONFIG--
+-------------------------------------------------------------------------------------------------------------
+config.initialized = true
---
+++ fxmanifest.lua
@@ -0,0 +1,18 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+description 'bloodyscreen'
+
+version '1.0'
+
+ui_page 'html/main.html'
+
+client_scripts {
+	'client/*.lua',
+	'config.lua'
+}
+
+files {
+    "html/*"
+}
